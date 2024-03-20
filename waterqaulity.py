ed87c8c3429512081a64c932355568096fe09dfd
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-03-20 18:57:58 +0530

    waterqaulity.py

--- waterqaulity.py
+++ waterqaulity.py
@@ -11,8 +11,8 @@ def load_model(model_path):
      
     return model
 
-def predict_portability(model, ph,hardness,solids,sulfate,cond,carbon,turb,trihalome):
-    input_features = [[ph,hardness,solids,sulfate,cond,carbon,turb,trihalome]]
+def predict_portability(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome):
+    input_features = [[ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome]]
     portabilty = model.predict(input_features)[0]
     return portability
 
@@ -27,6 +27,7 @@ def main():
     ph = st.number_input("Enter ph:", min_value=0.0, step=0.1)
     hardness = st.number_input("Enter hardness:", min_value=0.0, step=0.1)
     solids = st.number_input("Enter solids:", min_value=0.0, step=0.1)
+    cholarmine = st.number_input("Enter cholarmine:", min_value=0.0, step=0.1)
     sulfate = st.number_input("Enter sulfate:", min_value=0.0, step=0.1)
     cond= st.number_input("Enter conductivity:", min_value=0.0, step=0.1)
     carbon = st.number_input("Enter carbon:", min_value=0.0, step=0.1)
@@ -39,7 +40,7 @@ def main():
     # Button to predict power
     if st.button("Predict Power"):
         # Predict power
-        portability = predict_portability(model, ph,hardness,solids,sulfate,cond,carbon,turb,trihalome)
+        portability = predict_portability(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome)
         st.success(f"Predicted portability: {portablity}")
 
 if __name__ == "__main__":
