d596158da11be0f1fa396de21fdad3da7edbdeae
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-03-26 10:13:19 +0530

    waterqaulity.py

--- waterqaulity.py
+++ waterqaulity.py
@@ -4,26 +4,24 @@ import gzip
 from joblib import load
 
  
-def load_model(model_path):
-    model = load('random_forest_model881.joblib.gz')
-    
-     
-     
-    return model
+  
 
-def predict_portability(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome):
+def predict_portability1(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome):
     input_features = [[ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome]]
+    portability = model.predict(input_features)[0]
+    return portability
+def predict_portability2(model,Latitude,Longitude):
+    input_features = [[Latitude,Longitude]]
     portability = model.predict(input_features)[0]
     return portability
 
 def main():
-    st.title("water quality Prediction App")
-
-    # Load machine learning model
-    model_path = "random_forest_model8.joblib.gz"
-    model = load_model(model_path)
-
-    # User inputs
+ page=st.sidebar('Navigation',['Welcome','water quality','water depth'])
+ if page=='Welcome':
+  st.title("water quality Prediction App")
+  st.image("")
+elif page=='water_quality':
+     
     ph = st.number_input("Enter ph:", min_value=0.0, step=0.1)
     hardness = st.number_input("Enter hardness:", min_value=0.0, step=0.1)
     solids = st.number_input("Enter solids:", min_value=0.0, step=0.1)
@@ -33,15 +31,20 @@ def main():
     carbon = st.number_input("Enter carbon:", min_value=0.0, step=0.1)
     turb = st.number_input("Enter turb:", min_value=0.0, step=0.1)
     trihalome = st.number_input("Enter trihalome:", min_value=0.0, step=0.1)
-    
-    
-              
-
-    # Button to predict power
+    model = load('random_forest_model881.joblib.gz')
     if st.button("predicit"):
         # Predict power
-        portability = predict_portability(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome)
+        portability = predict_portability1(model, ph,hardness,solids,cholarmine,sulfate,cond,carbon,turb,trihalome)
         st.success(f"Predicted portability: {portability}")
+elif page=='water_depth':
+ Latitude = st.number_input("Enter latitude:")
+ Longitude= st.number_input("Enter longitude:")
+ model = load('random_forest_model880.joblib.gz')
+ if st.button("predicit"):
+        # Predict power
+        portability = predict_portability2(model,Latitude,Longitude)
+        st.success(f"Predicted portability: {portability}")
+ 
 
 if __name__ == "__main__":
     main()
