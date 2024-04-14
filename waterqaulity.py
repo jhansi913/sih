f4f6737032979129a6036a75d8affb9272d6fd88
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-04-14 10:29:40 +0530

    waterqaulity.py

--- waterqaulity.py
+++ waterqaulity.py
@@ -68,14 +68,14 @@ def main():
 
          
  elif page=='water_depth':
-  st.title(" WATER DEPTH")
+  st.title("Water Depth prediction")
   with open("home.html", "r", encoding="utf-8") as file1:
    html_content1 = file1.read()
   st.components.v1.html(html_content1, height=600, scrolling=True)
     
      
  
-  st.title("Water Depth prediction")
+   
   
   Latitude = st.number_input("Enter latitude:")
   Longitude= st.number_input("Enter longitude:")
