b9e848ea1d907c4e5fbf6c82203e4c9c82f17a57
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-04-26 22:25:23 +0530

    waterqaulity.py

--- waterqaulity.py
+++ waterqaulity.py
@@ -75,6 +75,7 @@ def main():
   else:
    st.write("No predictions made yet.")
  elif page=='final':
+  st.title("Samples of water quality")
   df = pd.read_csv('water_potability.csv')
   for column in df.columns:
     if df[column].dtype in [int, float]:  # Only fill numeric columns
@@ -84,7 +85,7 @@ def main():
   bottom_3 = df.tail(3)
   combined_df = pd.concat([top_3, bottom_3])
   st.dataframe(combined_df)
-  
+  st.title("Samples of water depth")
   df = pd.read_csv('Book11 .csv')
   for column in df.columns:
     if df[column].dtype in [int, float]:  # Only fill numeric columns
